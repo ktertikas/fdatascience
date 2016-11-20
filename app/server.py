1d5947d555190ac23130fd6a1eeaca4333745a7e
ktertikas <ktertikas@gmail.com>
2016-11-20 19:00:10 +0000

    MongoDB insert document inside a collection

--- app/server.py
+++ app/server.py
@@ -2,7 +2,13 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json, ast
 
 
 client = MongoClient('localhost',27017)
-db = client['fdatascience']
+db = client.fdatascience
+db.app.insert_one({
+	'name' : 'Top'
+	})
+top = db.app.find_one()

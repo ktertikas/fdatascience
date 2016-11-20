9c0700efce8f5e16a0b95f145c59557092af1290
ktertikas <ktertikas@gmail.com>
2016-11-20 19:22:07 +0000

    Created testcode folder and file, made small change to server.py

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-import tornado.ioloop
+﻿import tornado.ioloop
 import tornado.web
 import tornado.websocket
 from pymongo import MongoClient
@@ -8,7 +8,11 @@ import json, ast
 
 client = MongoClient('localhost',27017)
 db = client.fdatascience
-db.app.insert_one({
-	'name' : 'Top'
-	})
-top = db.app.find_one()
+# db.app.insert_one({
+# 	'name' : 'Top'
+# 	})
+top = db.app.find()
+
+# print top
+for i in top:
+	print i["name"]

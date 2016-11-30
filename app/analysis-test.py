1a9ad4c0a65d2b8bad78d45e8167b646fe2e6f00
ekawutsuw <ekawut.suw@outlook.com>
2016-11-30 21:08:03 +0000

    created lists analysis-test.py

--- app/analysis-test.py
+++ app/analysis-test.py
@@ -4,27 +4,54 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost',27017)
 db = client.fdatascience # create db name "fdatascience"
-# nutrients
-# consumption
-# beverages_consumption
 print(client.database_names())
 print(db.collection_names())
 
-sinfants = db.consumption.find( { "Population Class": "Infants" } )
-print(type(sinfants))
-infants = []
-for i in sinfants:
-	infants.append(i)
-print(infants)
-print(type(infants))
+# s_nutrients = db.nutrients.find()
+# print(type(s_nutrients))
+# nutrients = []
+# for i in s_nutrients:
+# 	nutrients.append(i)
+# print(nutrients)
+# print(type(nutrients))
+#Food Code
+
+s_con_infants = db.consumption.find( { "Population Class": "Infants" } )
+print(type(s_con_infants))
+con_infants = []
+for i in s_con_infants:
+	con_infants.append(i)
+# print(con_infants)
+# print(type(con_infants))
+f_con_infants = []
+for i in con_infants:
+	dic = {}
+	dic["PopClass"] = i["Population Class"]
+	dic["FoodName"] = i["Level 1 FoodEx Name"]
+	dic["Mean"] = float(i["Mean consumption in grams/ kg body weight per day"])
+	dic["Median"] = float(i["Median consumption in grams/ kg body weight per day"])
+	f_con_infants.append(dic)
+print(f_con_infants)
+
+s_bev_infants = db.beverages_consumption.find( { "Population Class": "Infants" } )
+print(type(s_bev_infants))
+bev_infants = []
+for i in s_bev_infants:
+	bev_infants.append(i)
+# print(bev_infants)
+# print(type(bev_infants))
 # Level 1 FoodEx Name
 # Mean consumption in grams/ kg body weight per day
 # Median consumption in grams/ kg body weight per day
+f_bev_infants = []
+for i in bev_infants:
+	dic = {}
+	dic["PopClass"] = i["Population Class"]
+	dic["FoodName"] = i["Level 1 FoodEx Name"]
+	dic["Mean"] = float(i["Mean consumption in grams/ kg body weight per day"])
+	dic["Median"] = float(i["Median consumption in grams/ kg body weight per day"])
+	f_bev_infants.append(dic)
+print(f_bev_infants)
 
 # mean_weights_inf = []
 # median_weights_inf = []
-
-for i in infants:
-	float(i["Median consumption in grams/ kg body weight per day"])
-	float(i["Mean consumption in grams/ kg body weight per day"])
-	i["Level 1 FoodEx Name"]

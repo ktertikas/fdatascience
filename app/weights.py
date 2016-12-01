c9033870097c1225c9c43a115f88b172107826be
ktertikas <ktertikas@gmail.com>
2016-12-01 00:09:43 +0000

    Created function for all age groups, clean data with mean weights and median weights

--- app/weights.py
+++ app/weights.py
@@ -1,13 +1,34 @@
 from pymongo import MongoClient
 import numpy as np
 
-def getDict ( aaa ) :
-	mean_weights_inf = []
-	median_weights_inf = []
-	for i in aaa:
-		median_weights_inf.append(float(i["Median consumption in grams/ kg body weight per day"]))
-		mean_weights_inf.append(float(i["Mean consumption in grams/ kg body weight per day"]))
-	x = np.array(mean_weights_inf)
-	y = np.array(median_weights_inf)
-	w_mean_inf = np.true_divide(x, x.sum())
-	w_median_inf = np.true_divide(y, y.sum())
+
+def getDict (  aaa ) :
+	client = MongoClient('localhost',27017)
+	db = client.fdatascience # create db name "fdatascience"
+	s_con = db.consumption.find(aaa)
+	con = []
+	for i in s_con:
+		con.append(i)
+	mean_weights = []
+	median_weights = []
+	for i in con:
+		median_weights.append(float(i["Median consumption in grams/ kg body weight per day"]))
+		mean_weights.append(float(i["Mean consumption in grams/ kg body weight per day"]))
+	x = np.array(mean_weights)
+	y = np.array(median_weights)
+	w_mean = np.true_divide(x, x.sum())
+	w_median = np.true_divide(y, y.sum())
+
+	f_con = []
+	count_row=0
+	for i in con:
+		dic = {}
+		dic["PopClass"] = i["Population Class"]
+		dic["FoodName"] = i["Level 1 FoodEx Name"]
+		dic["Mean"] = float(i["Mean consumption in grams/ kg body weight per day"])
+		dic["MeanWeight"] = w_mean[count_row]
+		dic["Median"] = float(i["Median consumption in grams/ kg body weight per day"])
+		dic["MedianWeight"] = w_median[count_row]
+		f_con.append(dic)
+		count_row+=1
+	print(f_con)

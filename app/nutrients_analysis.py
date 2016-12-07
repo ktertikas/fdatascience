c950007c6271e66abd83cf00667b1d52a4c7e48c
ekawutsuw <ekawut.suw@outlook.com>
2016-12-07 13:22:07 +0000

    find mean,median&sd for all group of nutrients

--- app/nutrients_analysis.py
+++ app/nutrients_analysis.py
@@ -3,7 +3,6 @@ import numpy as np
 import json
 
 import matplotlib.pyplot as plt
-from weights import getConDict, getBevDict
 
 client = MongoClient('localhost',27017)
 db = client.fdatascience # create db name "fdatascience"
@@ -30,18 +29,29 @@ nutr_keys = ['VitaminC', 'Fat', 'VitaminK', 'Nitrogen', 'Cholesterol', 'EnergyJ'
 # 	temp = np.append(temp, j['VitaminC'])
 # print(temp)
 
-final = np.array([])
-for i in nutr_keys:
-	nutr_key_value = db.nutrients_clean.find( {i:{'$ne':None}}, {'_id':0, i:1} )
-	temp = np.array([])
-	for j in nutr_key_value:
-		temp = np.append(temp, j[i])
-	# plt.hist(temp, bins=20)
-	# plt.savefig('hist_'+i+'.png')
-	# plt.clf()
-	# plt.boxplot(temp)
-	# plt.savefig('boxplot_'+i+'.png')
-	# plt.clf()
-	print("mean value of "+i+" is "+str(np.mean(temp)))
-	print("median value of "+i+" is "+str(np.median(temp)))
-	print("standard deviation value of "+i+" is "+str(np.std(temp)))
+nutrc_foodcode = db.nutrients_clean.distinct("FoodCode")
+for i in nutrc_foodcode:
+	file = open("nutrients_text_output/"+i+".txt", "wb")
+	for j in nutr_keys:
+		nutr_key_value = db.nutrients_clean.find( {j:{'$ne':None}, "FoodCode":i}, {'_id':0, j:1} )
+		temp = np.array([])
+		for k in nutr_key_value:
+			temp = np.append(temp, k[j])
+
+		print("Food Code in processing: "+i+" & Key->"+j)
+		plt.hist(temp, bins=20)
+		plt.savefig('nutrients_graphical_output/hist_'+i+'_'+j+'.png')
+		plt.clf()
+		# plt.boxplot(temp)
+		# plt.savefig('nutrients_graphical_output/boxplot_'+i+'_'+j+'.png')
+		# plt.clf()
+		mean_print = "mean value of "+j+" is ;"+str(np.mean(temp))
+		median_print = "median value of "+j+" is ;"+str(np.median(temp))
+		sd_print = "standard deviation value of "+j+" is ;"+str(np.std(temp))
+		file.write(mean_print.encode('utf-8'))
+		file.write(b"\n")
+		file.write(median_print.encode('utf-8'))
+		file.write(b"\n")
+		file.write(sd_print.encode('utf-8'))
+		file.write(b"\n")
+	file.close()

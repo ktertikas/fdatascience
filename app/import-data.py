5cae9cce73d11d51eea7b796f4d9b7e0d25194b7
ktertikas <ktertikas@gmail.com>
2016-12-08 22:45:16 +0000

    Changes and worries for analysis :p

--- app/import-data.py
+++ app/import-data.py
@@ -7,7 +7,7 @@ con_file = open('consumption.CSV')
 bev_file = open('beverages_consumption.CSV')
 nu_data = csv.DictReader(nu_file, delimiter=';')
 con_data = csv.DictReader(con_file, delimiter=';')
-bev_data = csv.DictReader(bev_file, delimiter=';')
+# bev_data = csv.DictReader(bev_file, delimiter=';')
 
 # count_break = 0 # test
 
@@ -15,7 +15,7 @@ client = MongoClient('localhost',27017)
 db = client.fdatascience # create db name "fdatascience"
 nu_coll = db.nutrients # create collection name "nutrients"
 con_coll = db.consumption # create collection name "consumption"
-bev_coll = db.beverages_consumption # create collection name "beverages_consumption"
+# bev_coll = db.beverages_consumption # create collection name "beverages_consumption"
 
 for i in nu_data:
 	# print(i)
@@ -25,9 +25,9 @@ for i in con_data:
 	# print(i)
 	con_coll.insert_one(i)
 
-for i in bev_data:
-	# print(i)
-	bev_coll.insert_one(i)
+# for i in bev_data:
+# 	# print(i)
+# 	bev_coll.insert_one(i)
 
 	# count_break+=1 # test
 	# if(count_break==2): # test
--- app/nutrients_analysis.py
+++ app/nutrients_analysis.py
@@ -30,28 +30,46 @@ nutr_keys = ['VitaminC', 'Fat', 'VitaminK', 'Nitrogen', 'Cholesterol', 'EnergyJ'
 # print(temp)
 
 nutrc_foodcode = db.nutrients_clean.distinct("FoodCode")
+final=[]
 for i in nutrc_foodcode:
 	file = open("nutrients_text_output/"+i+".txt", "wb")
+	dic={}
 	for j in nutr_keys:
 		nutr_key_value = db.nutrients_clean.find( {j:{'$ne':None}, "FoodCode":i}, {'_id':0, j:1} )
 		temp = np.array([])
 		for k in nutr_key_value:
 			temp = np.append(temp, k[j])
 
-		print("Food Code in processing: "+i+" & Key->"+j)
-		plt.hist(temp, bins=20)
-		plt.savefig('nutrients_graphical_output/hist_'+i+'_'+j+'.png')
-		plt.clf()
+		# print("Food Code in processing: "+i+" & Key->"+j)
+		# plt.hist(temp, bins=20)
+		# plt.savefig('nutrients_graphical_output/'+i+'_'+j+'.png')
+		# plt.clf()
 		# plt.boxplot(temp)
 		# plt.savefig('nutrients_graphical_output/boxplot_'+i+'_'+j+'.png')
 		# plt.clf()
-		mean_print = "mean value of "+j+" is ;"+str(np.mean(temp))
-		median_print = "median value of "+j+" is ;"+str(np.median(temp))
-		sd_print = "standard deviation value of "+j+" is ;"+str(np.std(temp))
-		file.write(mean_print.encode('utf-8'))
-		file.write(b"\n")
-		file.write(median_print.encode('utf-8'))
-		file.write(b"\n")
-		file.write(sd_print.encode('utf-8'))
-		file.write(b"\n")
+		# mean_print = "mean value of "+j+" is ;"+str(np.mean(temp))
+		# median_print = "median value of "+j+" is ;"+str(np.median(temp))
+		# sd_print = "standard deviation value of "+j+" is ;"+str(np.std(temp))
+		# file.write(mean_print.encode('utf-8'))
+		# file.write(b"\n")
+		# file.write(median_print.encode('utf-8'))
+		# file.write(b"\n")
+		# file.write(sd_print.encode('utf-8'))
+		# file.write(b"\n")
+		if j == "EnergyCal":
+			dic["FoodCode"] = i
+			dic[j] = np.median(temp)
+	final.append(dic)
 	file.close()
+# print(final)
+# find clean consumption for infants and take median weights for every food group
+consum = db.consumption_clean.find({"PopClass":"Infants"})
+for i in consum:
+	sum1 = 0
+	for j in final:
+		if i["FoodName"] == j["FoodCode"]:
+			print(i["MedianWeight"])
+			print(j["EnergyCal"])
+			sum1 = sum1 + i["MedianWeight"]*j["EnergyCal"]
+print(sum1)
+			

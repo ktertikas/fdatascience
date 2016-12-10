9a1fa7ea1d5de5c6c84cc1996b1c5f9c99a64d9d
ktertikas <ktertikas@gmail.com>
2016-12-10 22:01:24 +0000

    Monte Carlo code creates array for 1000 items

--- app/monte_carlo.py
+++ app/monte_carlo.py
@@ -1,18 +1,32 @@
 import random
 from pymongo import MongoClient
 import numpy as np
+from median_nutrients import getMedianNutr
+
+dic_nutr = getMedianNutr()
+print(dic_nutr["vegetables and vegetable products"])
 
 client = MongoClient('localhost',27017)
 db = client.fdatascience 
-infants = db.consumption_clean.find({"PopClass":"Infants"})
+infants = db.consumption_clean.find({"PopClass":"Adults"})
 number = infants.count()
 k = 0
+sample = 1000
+sample_arr = np.zeros((sample, number))
+names = []
+# print(sample_arr)
+# print(count)
 count = np.zeros(number)
-print(count)
 for i in infants:
-	for j in range(1, 1000):
+	for j in range(1, sample):
 			x = random.random()
 			if x <= i["Percentage"]:
+				sample_arr[j-1,k] = i["Median"]	
 				count[k] = count[k] + 1
+			else:
+				sample_arr[j-1,k] = 0.0
+	names.append(i["FoodName"])
 	k = k + 1
+print(sample_arr)
 print(count)
+print(names)

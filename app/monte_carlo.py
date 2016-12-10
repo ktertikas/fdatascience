a9c884043e671998adbda8c3f981a25dfcf7b556
ekawuts <ekawuts@XPS13-9350.localdomain>
2016-12-10 21:45:41 +0000

    created median_nutrients.py

--- app/monte_carlo.py
+++ app/monte_carlo.py
@@ -1,18 +1,22 @@
 import random
 from pymongo import MongoClient
 import numpy as np
+from median_nutrients import getMedianNutr
 
-client = MongoClient('localhost',27017)
-db = client.fdatascience 
-infants = db.consumption_clean.find({"PopClass":"Infants"})
-number = infants.count()
-k = 0
-count = np.zeros(number)
-print(count)
-for i in infants:
-	for j in range(1, 1000):
-			x = random.random()
-			if x <= i["Percentage"]:
-				count[k] = count[k] + 1
-	k = k + 1
-print(count)
+dic_nutr = getMedianNutr()
+print(dic_nutr['vegetables and vegetable products'])
+
+# client = MongoClient('localhost',27017)
+# db = client.fdatascience 
+# infants = db.consumption_clean.find({"PopClass":"Infants"})
+# number = infants.count()
+# k = 0
+# count = np.zeros(number)
+# print(count)
+# for i in infants:
+# 	for j in range(1, 1000):
+# 			x = random.random()
+# 			if x <= i["Percentage"]:
+# 				count[k] = count[k] + 1
+# 	k = k + 1
+# print(count)

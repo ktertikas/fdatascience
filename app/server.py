67cd407b0b31f261ff9e6adbbc390e95b521e743
ktertikas <ktertikas@gmail.com>
2016-12-14 20:11:24 +0000

    Fixed server that renders html and css js files

--- app/server.py
+++ app/server.py
@@ -3,6 +3,7 @@ import tornado.web
 import tornado.websocket
 import json
 from pymongo import MongoClient
+import os
 # from bson.objectid import ObjectId
 
 client = MongoClient('localhost',27017)
@@ -17,6 +18,27 @@ print(collection.find())
 
 # top = db.app.find_one()
 
-top = collection.find()
-for i in top:
-	print(i['name'])
+# top = collection.find()
+# for i in top:
+# 	print(i)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        print("GET / request from", self.request.remote_ip)
+        # self.write("aada")
+        self.render("index.html")
+
+handlers = [
+            (r"/", MainHandler)
+            ]
+
+settings = dict(
+	template_path=os.path.join(os.path.dirname(__file__), "templates"),
+	static_path=os.path.join(os.path.dirname(__file__), "static"),
+        )
+
+application = tornado.web.Application(handlers, **settings)
+
+application.listen(8888, '0.0.0.0')
+
+tornado.ioloop.IOLoop.instance().start()
